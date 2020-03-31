f748996d555a60e50c956e268e6683406813a8ed
JZielins <jzielinski156@gmail.com>
2020-03-31 11:32:50 +0200

    comments added; end

--- main.cpp
+++ main.cpp
@@ -4,42 +4,42 @@
 #include <vector>
 #include <algorithm>
 #include <random>
-#include <utility>
-#include <iterator>
-#include <cassert>
+
+
 
 std::vector<std::pair<char, int>> generateKey();
-std::string encode(std::string message, std::vector<std::pair<char, int>> key);
-std::string decode(std::string message, std::vector<std::pair<char, int>> key);
+std::string encrypt(std::string message, std::vector< std::pair<char, int> > key);
+std::string decrypt(std::string message, std::vector< std::pair<char, int> > key);
+
+
 
 int main() {
 
+
     std::string message = "Hello, Coders School!";
-    std::vector<char> s (message.begin(), message.end());
-
+    
     auto key = generateKey();
-    auto cypher = encode(message, key);
-    auto result = decode(cypher,key);
+    auto cypher = encrypt(message, key);
+    auto result = decrypt(cypher,key);
 
-    assert(message == result && "Something doesn't work");
 
-   
+    assert(message == result && "message != result");
     return 0;
 }
 
+/*
+    generateKey - fucntion generate a key, which is a vector of pairs with every letter, space, slash etc. (ASCII in range [32,126])
+    with shuffled ints witin the same range
+*/
 std::vector<std::pair<char, int>> generateKey()
 {
     std::random_device rd;
     std::mt19937 g(rd());
 
-    std::vector<int> keyInt(95);
-    std::vector<char> keyChar(95); 
+    std::vector<int> keyInt(95); 
 
-    std::generate(keyInt.begin(), keyInt.end(), [n=32] () mutable {return n++;});
-    std::shuffle(keyInt.begin(), keyInt.end(), g);
-
-    std::generate(keyChar.begin(), keyChar.end(), [n=32] () mutable {return n++;});
-    
+    std::generate(keyInt.begin(), keyInt.end(), [n=32] () mutable {return n++;});   
+    std::shuffle(keyInt.begin(), keyInt.end(), g);                      
 
     std::vector< std::pair<char, int> > key;
    
@@ -51,15 +51,18 @@ std::vector<std::pair<char, int>> generateKey()
     return key;
 }
 
-std::string encode(std::string message, std::vector<std::pair<char, int>> key)
+/*
+    encrypt - for every letter in 'message', this function is searching for it's equivalent in generated key and change it's value 
+*/
+std::string encrypt(std::string message, std::vector<std::pair<char, int>> key)
 {
 
     for(int i = 0; i < message.size(); i++)
     {
         char temp = message[i];
         auto it = std::find_if(key.begin(), key.end(), 
+        [temp](const auto key) { return key.first == temp; } );
 
-        [temp]( const auto key) { return key.first == temp; } );
         if(it != key.end())
             { message[i] = it->second; }
     }
@@ -67,15 +70,18 @@ std::string encode(std::string message, std::vector<std::pair<char, int>> key)
     return message;
 }
 
-std::string decode(std::string message, std::vector<std::pair<char, int>> key)
+/*
+    encrypt - for every letter in encypted 'message' function is searching for it's equivalent in key and change it for it's original value
+*/
+std::string decrypt(std::string message, std::vector<std::pair<char, int>> key)
 {
 
     for(int i = 0; i < message.size(); i++)
     {
         char temp = message[i];
         auto it = std::find_if(key.begin(), key.end(), 
-        
-        [temp]( const auto key) { return key.second == temp; } );
+        [temp](const auto key) { return key.second == temp; } );
+
         if(it != key.end())
             { message[i] = it->first; }
     }
